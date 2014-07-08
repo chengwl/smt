bd1fa1775f0ec273fe56b1e8e65e73a21fbdf755
cwl <cwllovejava@163.com>
2014-07-08 21:43:10 +0800

    集成mybatis

--- src/main/java/com/cwl/smt/web/AbspTest.java
+++ src/main/java/com/cwl/smt/web/AbspTest.java
@@ -1,15 +1,29 @@
 package com.cwl.smt.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.cwl.smt.model.UserInfo;
+import com.cwl.smt.model.UserInfoMapper;
 
 @Controller
 public class AbspTest {
+	
+	@Autowired
+	UserInfoMapper userInfoMapper;
+	UserInfo userInfo;
 
 	@RequestMapping(value = "index1")
 	public String testGoSuccessJsp() {
 		System.out.println("***********************");
+		
+		List<UserInfo> userInfos = userInfoMapper.selectByExample(null);
+		
+		System.out.println(userInfos.size());
+		
 		return "index1";
 	}
 
