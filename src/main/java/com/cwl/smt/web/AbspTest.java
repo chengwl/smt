cc817d62276e3948f93674ca90afbb6cefd71c76
cwl <cwllovejava@163.com>
2014-07-09 11:14:59 +0800

    整理配置文件，并添加log配置

--- src/main/java/com/cwl/smt/web/AbspTest.java
+++ src/main/java/com/cwl/smt/web/AbspTest.java
@@ -2,6 +2,8 @@ package com.cwl.smt.web;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import com.cwl.smt.model.UserInfoMapper;
 @Controller
 public class AbspTest {
 	
+	Logger LOG = LoggerFactory.getLogger(AbspTest.class);
+	
 	@Autowired
 	UserInfoMapper userInfoMapper;
 	UserInfo userInfo;
@@ -19,6 +23,8 @@ public class AbspTest {
 	@RequestMapping(value = "index1")
 	public String testGoSuccessJsp() {
 		System.out.println("***********************");
+		
+		LOG.debug("*******AbspTest test*************");
 		
 		List<UserInfo> userInfos = userInfoMapper.selectByExample(null);
 		
